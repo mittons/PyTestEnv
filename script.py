8ca42a873c760ca543c54eb44ffe635e7bcf4f23
Axel Gauti <icyaxel@gmail.com>
2023-11-29 13:22:48 +0000

    first commit2

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    test_var = os.getenv('TEST_VAR', 'Not the right value')
+    test_var = os.getenv('TEST_VAR', 'Not the right value!')
     print(f"Env var found: {test_var}")
     
 if __name__ == "__main__":
