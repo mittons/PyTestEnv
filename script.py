e829d00f69c5aa8da7b5c53d76ab5a1a5e1983d5
Axel Gauti <icyaxel@gmail.com>
2023-11-29 13:39:58 +0000

    fifth commit

--- script.py
+++ script.py
@@ -2,7 +2,10 @@ import os
 
 def main():
     test_var = os.getenv('TEST_VAR', 'Not the right value!?')
-    print(f"Env var found: {test_var}")
+    s0 = test_var[0]
+    s1 = test_var[1:]
+    print(f"Env var found: {s0}")
+    print(f"Env var found: {s1}")
     
 if __name__ == "__main__":
     main()
